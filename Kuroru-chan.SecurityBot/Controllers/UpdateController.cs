9e3fa98e40629a0977b7bc5e5a221b330595cd32
SamLangTen <samlangten@outlook.com>
2019-08-16 11:07:37 +0800

    add dockerfile

---
+++ Kuroru-chan.SecurityBot/Controllers/UpdateController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KuroruChan.SecurityBot.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace Kuroru_chan.SecurityBot.Controllers
+{
+    [Route("api/[controller]")]
+    public class UpdateController : Controller
+    {
+        private readonly IUpdateService _updateService;
+
+        public UpdateController(IUpdateService updateService)
+        {
+            _updateService = updateService;
+        }
+
+        // POST api/update
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Update update)
+        {
+            await _updateService.EchoAsync(update);
+            return Ok();
+        }
+    }
+}
